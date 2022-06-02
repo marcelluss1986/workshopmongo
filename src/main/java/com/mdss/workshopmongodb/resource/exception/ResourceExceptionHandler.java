c9a0f0ec88f89a6d71896fe9f9fba5c0e3cc5669
Marcelo Soares <marcelossoares@live.com>
2022-06-02 05:29:12 -0300

    findUserById

---
+++ src/main/java/com/mdss/workshopmongodb/resource/exception/ResourceExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.mdss.workshopmongodb.resource.exception;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.mdss.workshopmongodb.services.exception.ObjectNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	@ExceptionHandler
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Not found!", e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+}
